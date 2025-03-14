41e783110f0a54774065a50d12bb82bf8130e6f8
Seu Novo Nome <pauloahouagi@gmail.com>
2025-03-14 11:25:52 -0300

    encoding da senha

--- fakepinterest/__init__.py
+++ fakepinterest/__init__.py
@@ -5,7 +5,7 @@ from flask_bcrypt import Bcrypt
 import os
 
 app = Flask(__name__)
-#app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///comunidade.db"
+#app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://banco_fakepinterest_jbrq_user:XZXwfcLaj5h4wRDk117T8NwIL87GKhQA@dpg-cva33utumphs73adigfg-a.oregon-postgres.render.com/banco_fakepinterest_jbrq"
 app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL")
 app.config["SECRET_KEY"] = "3c7a3c7127025d810a4a4145da0535b4"
 app.config["UPLOAD_FOLDER"] = "static/fotos_posts"
